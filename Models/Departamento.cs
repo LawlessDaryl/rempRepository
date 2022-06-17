e9f51a398331347d337f252b6c8a00b120674f66
Quisbert G <h666heavymetal@gmail.com>
2022-06-16 23:18:54 -0400

    Modelo departamento

---
+++ Models/Departamento.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace remp.Models
+{
+    public class Departamento
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string descripción { get; set; }
+    }
+}
